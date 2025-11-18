d3bdfa4a974dd7b812f7167045f8722282be1f05
Mayank Pratap Singh <prmayanksingh05@gmail.com>
2025-11-18 13:52:03 +0530

    minor change

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const path = require("path")
 
 // Routes
 const authRoutes = require("./routes/auth.routes");
@@ -28,9 +29,16 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json());
+app.use(express.static(path.join(__dirname,"../public")))
+
 
 // Using Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
+
+
+app.get("*name", (req,res)=>{
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+})
 
 module.exports = app;
